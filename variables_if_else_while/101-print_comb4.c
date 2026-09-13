757da053643c2fb8f9e1b8a82580d22b2ce923eb
Peniel-source <p.obeng@alustudent.com>
2026-09-13 18:09:35 +0200

    3 digit combination

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int dig1;
+	int dig2;
+	int dig3;
+
+	for (dig1 = 0; dig1 <= 7; dig1++)
+	{
+		for (dig2 = dig1 + 1; dig2 <= 8; dig2++)
+		{
+			for (dig3 = dig2 + 1; dig3 <= 9; dig3++)
+			{
+				putchar(dig1 + '0');
+				putchar(dig2 + '0');
+				putchar(dig3 + '0');
+				if (dig1 != 7 || dig2 != 8 || dig3 != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
